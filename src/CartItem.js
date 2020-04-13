5aa54fed8afd5f4e930792bd5d0661fbec8b4e8d
Ambika Kumari <kambika1309@gmail.com>
2020-04-13 20:32:52 +0530

    added the function of increasing items and some css

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,24 +9,46 @@ class CartItem extends React.Component{
             qty:1.,
             img:''
         }
+        //to bind the "this" to increaseQuantity function
+        //this.increaseQuantity=this.increaseQuantity.bind(this);
+    }
+    //pointer function keep the reference of the object 
+    increaseQuantity=()=>{
+        console.log("print");
     }
     render(){
         //object disstructuring
         const{price,title,qty}=this.state;
         return(
         <div className="cart-item">
-        <div className="left-block"> 
-            <img style={styles.image}/>
-        </div> 
-        <div className="right-block">
-        <div style={{fontSize:25}}>{title}</div>
-        <div style={{color:'#777'}}>{price}</div>
-        <div style={{color:'#777'}}>{qty}</div>
-        </div>
-        <div class="cart-item-actions">
-            <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/jpg/03/23/33/19/500_F_323331925_VvJaAQ5sZz8VoEn5qDYGkB7Wu4O80zKL.jpg"/>
-            <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828817.svg"/>
-            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1632/1632602.svg"/>
+            <div className="left-block"> 
+                <img style={styles.image}/>
+            </div> 
+            <div className="right-block">
+                <div style={{fontSize:25}}>{title}</div>
+                <div style={{color:'#777'}}>{price}</div>
+                <div style={{color:'#777'}}>{qty}</div>
+            </div>
+            <div className="cart-item-actions">
+            <img 
+                alt="increase"
+                className="action-icons" 
+                src="https://image.flaticon.com/icons/svg/1828/1828817.svg"
+                onClick={this.increaseQuantity}
+            />
+
+            <img
+                alt="decrease" 
+                className="action-icons" 
+                src="https://as2.ftcdn.net/jpg/03/23/33/19/500_F_323331925_VvJaAQ5sZz8VoEn5qDYGkB7Wu4O80zKL.jpg"
+                
+            />
+
+            <img 
+                alt="delete" 
+                className="action-icons" 
+                src="https://image.flaticon.com/icons/svg/1632/1632602.svg"
+            />
         </div>
         </div>
         );
