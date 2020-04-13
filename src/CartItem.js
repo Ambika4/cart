5f50f550cac1a92181b939d1ca95ec2ce230f5de
Ambika Kumari <kambika1309@gmail.com>
2020-04-13 19:54:54 +0530

    added javascript disstructuring

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:999,
+            title:'Phone',
+            qty:1.,
+            img:''
+        }
+    }
     render(){
+        //object disstructuring
+        const{price,title,qty}=this.state;
         return(
         <div className="cart-item">
         <div className="left-block"> 
             <img style={styles.image}/>
         </div> 
         <div className="right-block">
-            <div style={{fontSize:25}}>Phone</div>
-            <div style={{color:'#777'}}>Rs 9999</div>
-            <div style={{color:'#777'}}>Quantity 1</div>
+        <div style={{fontSize:25}}>{title}</div>
+        <div style={{color:'#777'}}>{price}</div>
+        <div style={{color:'#777'}}>{qty}</div>
         </div>
         <div class="cart-item-actions">
-            {/*Buttons */}
+            <img alt="increase" className="action-icons" src="https://as2.ftcdn.net/jpg/03/23/33/19/500_F_323331925_VvJaAQ5sZz8VoEn5qDYGkB7Wu4O80zKL.jpg"/>
+            <img alt="decrease" className="action-icons" src="https://image.flaticon.com/icons/svg/1828/1828817.svg"/>
+            <img alt="delete" className="action-icons" src="https://image.flaticon.com/icons/svg/1632/1632602.svg"/>
         </div>
         </div>
         );
